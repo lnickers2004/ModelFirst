f77a0e9b6aadf2a2b6ea958e38da00d88d0ca1cd
lnickers2004 <lnickers@gmail.com>
2013-06-18 15:51:14 -0500

    Added ModelFirstRexipes.edmx

---
+++ ModelFirst/ModelFirstRecipes.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'Z:\Dropbox2\Dropbox\visual_studio_11_Projects\ENTITY_FRAMEWORK_PROJECTS\Fluent EF\Source Code\ModelFirst\ModelFirst\ModelFirstRecipes.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
